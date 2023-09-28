3e2db144b2414b82e56e9ac56e92ab7675a48f2c
Sarthakkk123 <sarthaksonawane147@gmail.com>
2023-09-28 11:37:43 +0530

    28-september

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,33 +1,41 @@
-import React from 'react';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
 
 const Head = () => {
+  const dispatched = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatched(toggleMenu());
+  };
+
   return (
-    <div className='header'>
-      <div className='header-left'>
+    <div className="header">
+      <div
+        className="header
+      -left"
+      >
         <img
-          className='menu-icon'
-          src='https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp'
-          alt='menu'
+          onClick={() => toggleMenuHandler()}
+          className="menu-icon"
+          src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"
+          alt="menu"
         />
         <img
-          className='youtube-logo'
-          src='https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500'
-          alt='youtube'
+          className="youtube-logo"
+          src="https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500"
+          alt="youtube"
         />
       </div>
-      <div className='header-center'>
-        <input
-          type='text'
-          className='search-input'
-          placeholder='Search'
-        />
-        <button className='search-button'>Search</button>
+      <div className="header-center">
+        <input type="text" className="search-input" placeholder="Search" />
+        <button className="search-button">Search</button>
       </div>
-      <div className='header-right'>
+      <div className="header-right">
         <img
-          className='user-icon'
-          src='https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png'
-          alt='user'
+          className="user-icon"
+          src="https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png"
+          alt="user"
         />
       </div>
     </div>
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import appReducer from "./appSlice"; // Import the reducer, not the slice object
 
-const store = configureStore({});
+const store = configureStore({
+  reducer: {
+    app: appReducer, // Use the appReducer here
+  },
+});
 
 export default store;
